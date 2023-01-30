9ef97040f395e15625d13448d128785d7c7043a6
yamasaki <yamasakipann0218@gmail.com>
2023-01-31 03:39:33 +0900

    main

---
+++ 1/src/cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 1. 8080番portのLisnterを作成
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	// 2. gRPCサーバーを作成
+	s := grpc.NewServer()
+
+	// 3. 作成したgRPCサーバーを、8080番ポートで稼働させる
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	// 4.Ctrl+Cが入力されたらGraceful shutdownされるようにする
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
